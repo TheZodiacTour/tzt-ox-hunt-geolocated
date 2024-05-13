8bd8ef474652fc5afe898d7d3284c72dce0a9301
TheZodiacTour <thezodiactour@gmail.com>
2024-05-13 03:38:58 -0400

    Update firebase.service.js
    nam5 line 25

--- client/src/services/firebase.service.js
+++ client/src/services/firebase.service.js
@@ -22,7 +22,7 @@ class FirebaseService {
         // Initialize Firebase
         const fireApp = initializeApp(firebaseConfig);
         const analytics = getAnalytics(app);
-        this.fireFunctions = getFunctions(fireApp, 'united-states');
+        this.fireFunctions = getFunctions(fireApp, 'nam5');
         
         if ('development' === Constants.ENV) {
             console.log("connected to emulator");
